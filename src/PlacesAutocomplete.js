293e109fcdced528279a894c26ba6295619bf1a0
Nick Barrett <nick@juicebox.com.au>
2018-05-16 10:14:08 +0800

    allow passing arbitrary props to render method

--- src/PlacesAutocomplete.js
+++ src/PlacesAutocomplete.js
@@ -352,6 +352,7 @@ class PlacesAutocomplete extends React.Component {
       getInputProps: this.getInputProps,
       getSuggestionItemProps: this.getSuggestionItemProps,
       suggestions: this.state.suggestions,
+      ...this.props,
     });
   }
 }
